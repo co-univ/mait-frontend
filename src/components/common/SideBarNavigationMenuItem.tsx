f93d3a35d60c1e2792e5679a561f7bcaaf74fc31
MinJaeSon <mjsonsj@gmail.com>
2025-07-26 17:57:19 +0900

    style: apply clsx for conditional styling

--- src/components/common/SideBarNavigationMenuItem.tsx
+++ src/components/common/SideBarNavigationMenuItem.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import type React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -35,13 +36,23 @@ const SideBarNavigationMenuItem = ({
 	return (
 		<button type="button" onClick={handleMenuClick}>
 			<div
-				className={`group flex items-center gap-[0.625rem] p-3 rounded-[0.375rem] hover:bg-primary-5 ${selected ? "bg-primary-5" : ""}`}
+				className={clsx(
+					"group flex items-center gap-[0.625rem] p-3 rounded-[0.375rem] hover:bg-primary-5",
+					{ "bg-primary-5": selected },
+				)}
 			>
 				{icon(
-					`w-5 h-5 ${selected ? "text-primary-50" : "text-gray-30 group-hover:text-primary-50"}`,
+					clsx("w-5 h-5", {
+						"text-primary-50": selected,
+						"text-gray-30 group-hover:text-primary-50": !selected,
+					}),
 				)}
 				<span
-					className={`text-base ${selected ? "text-primary-50 font-bold" : "text-gray-30 group-hover:text-primary-50 group-hover:font-bold"}`}
+					className={clsx("text-base", {
+						"text-primary-50 font-bold": selected,
+						"text-gray-30 group-hover:text-primary-50 group-hover:font-bold":
+							!selected,
+					})}
 				>
 					{label}
 				</span>
