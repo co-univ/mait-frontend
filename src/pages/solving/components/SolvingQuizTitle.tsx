56b237fc5ab02f0be4b4141692a07f5f073f807d
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-07-31 16:30:49 +0900

    feat: publish quiz title component

---
+++ src/pages/solving/components/SolvingQuizTitle.tsx
@@ -0,0 +1,29 @@
+//
+//
+//
+
+interface SolvingQuizTitleProps {
+	title: string;
+	type?: "molu";
+}
+
+const SolvingQuizTitle = ({ title, type = "molu" }: SolvingQuizTitleProps) => {
+	const lines = title.split("\n|\r");
+
+	console.log(lines);
+	return (
+		<div className="w-full">
+			<span className="typo-heading-small">
+				{lines.map((line, index) => (
+					// biome-ignore lint/suspicious/noArrayIndexKey: Using index as key is acceptable here since lines are static
+					<span key={index}>
+						{line}
+						{index < lines.length - 1 && <br />}
+					</span>
+				))}
+			</span>
+		</div>
+	);
+};
+
+export default SolvingQuizTitle;
--- src/pages/solving/pages/Solving.tsx
+++ src/pages/solving/pages/Solving.tsx
@@ -1,4 +1,5 @@
 import { ChevronRight, Puzzle } from "lucide-react";
+import SolvingQuizTitle from "../components/SolvingQuizTitle";
 import SolvingLayout from "../layouts";
 import SolvingControls from "./SolvingControls";
 import SolvingHeader from "./SolvingHeader";
@@ -10,7 +11,7 @@ import SolvingHeader from "./SolvingHeader";
 const Solving = () => {
 	return (
 		<SolvingLayout>
-			<div className="sticky top-0">
+			<div className="sticky top-0 bg-alpha-white100">
 				<SolvingHeader title="문제 해결" percentage={20} />
 				<div className="h-size-height-5" />
 				<SolvingControls
@@ -20,7 +21,10 @@ const Solving = () => {
 					buttonIcon={<ChevronRight className="stroke-primary-50" />}
 				/>
 			</div>
-			<div className="h-[1200px]" />
+			<div className="h-size-height-5" />
+			<div>
+				<SolvingQuizTitle title="GraphQL은 모든 요청을 동일한 () 로 보내며, 요청 방식도 post로 통일되어 있다. 빈칸에 들어갈 말을 고르세요." />
+			</div>
 		</SolvingLayout>
 	);
 };
