5cf20b104b7f24526e63b2e00203cf6b223dd448
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-08-05 16:20:10 +0900

    feat: implement correct omponent

--- src/pages/solving/pages/Solving.tsx
+++ src/pages/solving/pages/Solving.tsx
@@ -1,3 +1,6 @@
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
+import SolvingQuizCorrect from "../components/SolvingQuizCorrect";
 import SolvingLayout from "../layouts/SolvingLayout";
 import SolvingQuizContent from "./solving-quiz-content";
 import SolvingTopBar from "./solving-top-bar";
@@ -7,8 +10,36 @@ import SolvingTopBar from "./solving-top-bar";
 //
 
 const Solving = () => {
+	const [showCorrect, setShowCorrect] = useState(false);
+	const [isCorrect, setIsCorrect] = useState(true);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setShowCorrect(true);
+		}, 1000);
+
+		return () => clearTimeout(timer);
+	}, []);
+
+	const handleAnimationComplete = () => {
+		const hideTimer = setTimeout(() => {
+			setShowCorrect(false);
+		}, 2000);
+
+		return () => clearTimeout(hideTimer);
+	};
+
 	return (
 		<SolvingLayout>
+			<AnimatePresence mode="wait">
+				{showCorrect && (
+					<SolvingQuizCorrect
+						correct={isCorrect}
+						show={showCorrect}
+						onAnimationComplete={handleAnimationComplete}
+					/>
+				)}
+			</AnimatePresence>
 			<SolvingTopBar />
 			<div className="h-size-height-5" />
 			<SolvingQuizContent />
