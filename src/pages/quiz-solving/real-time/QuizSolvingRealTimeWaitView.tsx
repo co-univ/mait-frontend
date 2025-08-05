b7c6182d69d5bf89affd7e3c6dd09db73ee5c972
MinJaeSon <mjsonsj@gmail.com>
2025-08-05 22:29:22 +0900

    chore: rename wait view

--- src/pages/quiz-solving/real-time/QuizSolvingRealTimeWaitView.tsx
+++ src/pages/quiz-solving/real-time/QuizSolvingRealTimeWaitView.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 //
 //
 //
 
-const WaitView = () => {
+const QuizSolvingRealTimeWaitView = () => {
 	return (
 		<div className="bg-gradient-to-b from-alpha-white100 to-primary-5 flex justify-center items-center w-screen h-screen fixed top-0 left-0">
 			<div className="p-padding-12 w-[68.5rem] h-[5.875rem] flex items-center justify-center rounded-radius-medium1 bg-primary-5">
@@ -16,4 +17,4 @@ const WaitView = () => {
 	);
 };
 
-export default WaitView;
+export default QuizSolvingRealTimeWaitView;
