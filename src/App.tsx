cc1aaa5a9768a3be2a1c386720acf190fd2afe67
JO WONYOUNG <whdnjsdud551@gmail.com>
2025-08-07 21:24:01 +0900

    chore: add SolvingNextStage to App for testing

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,46 @@ import { Outlet, useLocation } from "react-router-dom";
 import AppLayout from "./components/common/AppLayout";
 import type { SIDEBAR_VARIANT } from "./components/common/SideBar";
 import Toast from "./components/common/Toast";
+import SolvingNextStage from "./pages/solving/pages/SolvingNextStage";
 
 //
 //
 //
 
 const App = () => {
-  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+	const [isSideBarOpen, setIsSideBarOpen] = useState(true);
 
-  const location = useLocation();
+	const location = useLocation();
 
-  const sidebarVariant: SIDEBAR_VARIANT = location.pathname.startsWith(
-    "/quiz-solving",
-  )
-    ? "overlay"
-    : "default";
+	const sidebarVariant: SIDEBAR_VARIANT = location.pathname.startsWith(
+		"/quiz-solving",
+	)
+		? "overlay"
+		: "default";
 
-  return (
-    <div className="app-container">
-      <Toast />
-      <AppLayout
-        isSideBarOpen={isSideBarOpen}
-        setIsSideBarOpen={setIsSideBarOpen}
-        variant={sidebarVariant}
-      />
-      <main
-        className={clsx(
-          "main-content transition-all duration-300 ease-in-out",
-          {
-            "pl-[calc(17.5rem+2rem)] pr-8":
-              isSideBarOpen && sidebarVariant === "default",
-            "px-[10.75rem]": !isSideBarOpen && sidebarVariant === "default",
-          },
-        )}
-      >
-        <Outlet />
-      </main>
-    </div>
-  );
+	return (
+		<div className="app-container">
+			<Toast />
+			<AppLayout
+				isSideBarOpen={isSideBarOpen}
+				setIsSideBarOpen={setIsSideBarOpen}
+				variant={sidebarVariant}
+			/>
+			<main
+				className={clsx(
+					"main-content transition-all duration-300 ease-in-out",
+					{
+						"pl-[calc(17.5rem+2rem)] pr-8":
+							isSideBarOpen && sidebarVariant === "default",
+						"px-[10.75rem]": !isSideBarOpen && sidebarVariant === "default",
+					},
+				)}
+			>
+				<SolvingNextStage open />
+				<Outlet />
+			</main>
+		</div>
+	);
 };
 
 export default App;
