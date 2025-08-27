fd6771886f137a95681e49cf2b8646963ca3bd3f
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-08-27 22:39:01 +0900

    fix: set header height

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -5,3 +5,4 @@ export const LARGE_PAGE_MARGIN = 172;
 export const SIDEBAR_WIDTH = "300px";
 export const SIDEBAR_TRANSITION =
 	"transition-all duration-300 transition-ease-in-out";
+export const HEADER_HEIGHT = 96;
