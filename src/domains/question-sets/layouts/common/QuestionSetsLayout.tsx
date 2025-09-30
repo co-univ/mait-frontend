45e18baa7f72f83dcbd7ea1f368c55426a4ad27a
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-10-01 03:06:40 +0900

    chore: set bottom margin in question set layout

--- src/domains/question-sets/layouts/common/QuestionSetsLayout.tsx
+++ src/domains/question-sets/layouts/common/QuestionSetsLayout.tsx
@@ -15,7 +15,7 @@ interface QuestionSetsLayoutPops {
 
 const QuestionSetsLayout = ({ children }: QuestionSetsLayoutPops) => {
 	return (
-		<div className="flex flex-col gap-gap-11 pt-padding-12">
+		<div className="flex flex-col gap-gap-11 py-padding-12">
 			<div className="flex gap-gap-5 items-center">
 				<SquarePen size={32} />
 				<h1 className="typo-heading-medium">문제 관리</h1>
