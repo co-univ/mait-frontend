58aaa7f33ecbf66cad237a644c85baf54928376e
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-10-02 16:29:10 +0900

    fix: apply app route file name change

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from "@/App.routes";
+import router from "@/app.routes";
 import { ConfirmProvider } from "@/components/confirm";
 
 const queryClient = new QueryClient({
