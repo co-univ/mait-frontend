0e1f91b0587ea70f329e1065e881d02082cc626b
JO WONYOUNG <whdnjsdud551@gmail.com>
2025-11-06 01:58:49 +0900

    feat: add variant prop to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,6 +7,7 @@ import type React from "react";
 
 export interface ButtonProps {
 	className?: string;
+	variant?: "primary" | "secondary";
 	icon?: React.ReactNode;
 	item?: React.ReactNode;
 	onClick?: () => void;
@@ -16,13 +17,17 @@ export interface ButtonProps {
 //
 //
 
-const Button = ({ className, icon, item, onClick }: ButtonProps) => {
+const Button = ({ className, variant, icon, item, onClick }: ButtonProps) => {
 	return (
 		<button
 			type="button"
 			className={clsx(
 				"p-padding-6 flex gap-gap-5 rounded-medium1 border border-color-gray-10 typo-body-medium",
 				className,
+				variant === "primary" &&
+					"hover:bg-color-primary-50 hover:border-color-primary-50 hover:text-color-alpha-white100",
+				variant === "secondary" &&
+					"hover:bg-color-secondary-50 hover:border-color-secondary-50 hover:text-color-alpha-white100",
 			)}
 			onClick={onClick}
 		>
