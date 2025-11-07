deb94591110327447a40992b193f69b8a561cb01
MinJaeSon <mjsonsj@gmail.com>
2025-11-07 13:27:23 +0900

    chore: file rename

--- src/domains/auth/components/AuthGoogleLoginButton.tsx
+++ src/domains/auth/components/AuthGoogleLoginButton.tsx
@@ -4,9 +4,17 @@ import googleLogo from "@/assets/images/google-logo.png";
 //
 //
 
-const AuthSocialLoginButton = () => {
+interface AuthSocialLoginButtonProps {
+	onClick: () => void;
+}
+
+//
+//
+//
+
+const AuthGoogleLoginButton = ({ onClick }: AuthSocialLoginButtonProps) => {
 	return (
-		<button type="button" className="w-full">
+		<button type="button" className="w-full" onClick={onClick}>
 			<div className="flex items-center py-padidng-8 px-padding-10 gap-[10px] bg-[#F2F2F2] rounded-md w-full max-w-[385px] h-size-height-10 select-none">
 				<img
 					src={googleLogo}
@@ -23,4 +31,4 @@ const AuthSocialLoginButton = () => {
 	);
 };
 
-export default AuthSocialLoginButton;
+export default AuthGoogleLoginButton;
