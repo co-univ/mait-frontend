32688a822dc640cad4849e049a9b16c909985a45
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-11-20 02:08:52 +0900

    chore: remove legacy login modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import { Outlet } from "react-router-dom";
-import LoginModal from "@/components/auth/LoginModal";
 import AppLayout from "@/layouts/AppLayout";
 import Toast from "./components/Toast";
-import useLoginModalOpenStore from "./stores/useLoginModalOpenStore";
 
 //
 //
 //
 
 const App = () => {
-	const { isLoginModalOpen, closeLoginModal } = useLoginModalOpenStore();
-
 	return (
 		<>
 			<AppLayout>
 				<Outlet />
 			</AppLayout>
-			<LoginModal open={isLoginModalOpen} onClose={closeLoginModal} />
 			<Toast />
 		</>
 	);
