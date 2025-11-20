a7527cc1b888945ee0127975802c67b5265886f5
MinJaeSon <mjsonsj@gmail.com>
2025-11-20 22:46:48 +0900

    feat: add protected route and apply to my page

--- src/domains/my-page/mypage.routes.tsx
+++ src/domains/my-page/mypage.routes.tsx
@@ -1,8 +1,13 @@
+import { ProtectedRoute } from "@/ProtectedRoute";
 import MyPage from "./pages/MyPage";
 
 export const myPageRoutes = [
 	{
 		path: "/mypage",
-		element: <MyPage />,
+		element: (
+			<ProtectedRoute>
+				<MyPage />
+			</ProtectedRoute>
+		),
 	},
 ];
