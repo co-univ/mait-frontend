46310257071264715321889e9687ab950c5274b8
JO WONYOUNG <whdnjsdud551@gmail.com>
2025-12-02 13:20:54 +0900

    refactor: apply guard pattern to home routes

--- src/domains/home/home.routes.tsx
+++ src/domains/home/home.routes.tsx
@@ -2,9 +2,24 @@ import { lazy } from "react";
 
 const Home = lazy(() => import("./pages/common/Home"));
 
+//
+//
+//
+
+/**
+ * @property {string} ROOT `/`
+ */
+export const HOME_ROUTE_PATH = {
+	ROOT: "/",
+};
+
+//
+//
+//
+
 export const homeRouter = [
 	{
-		path: "/",
+		path: HOME_ROUTE_PATH.ROOT,
 		element: <Home />,
 	},
 ];
