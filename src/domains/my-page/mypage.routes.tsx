57ecbb91cd62fee42a759b50a4afc5a8dbf770a0
JO WONYOUNG <whdnjsdud551@gmail.com>
2025-12-02 13:21:21 +0900

    refactor: apply guard pattern to mypage routes

--- src/domains/my-page/mypage.routes.tsx
+++ src/domains/my-page/mypage.routes.tsx
@@ -1,15 +1,30 @@
 import { lazy } from "react";
-import { ProtectedRoute } from "@/ProtectedRoute";
+import PublicOnlyGuard from "@/guards/PublicOnlyGuard";
 
 const MyPage = lazy(() => import("./pages/MyPage"));
 
+//
+//
+//
+
+/**
+ * @property {string} ROOT `/mypage`
+ */
+export const MYPAGE_ROUTE_PATH = {
+	ROOT: "/mypage",
+};
+
+//
+//
+//
+
 export const myPageRouter = [
 	{
-		path: "/mypage",
+		path: MYPAGE_ROUTE_PATH.ROOT,
 		element: (
-			<ProtectedRoute>
+			<PublicOnlyGuard>
 				<MyPage />
-			</ProtectedRoute>
+			</PublicOnlyGuard>
 		),
 	},
 ];
