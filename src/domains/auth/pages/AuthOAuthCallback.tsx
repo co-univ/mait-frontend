8891f99bbdbfd3b57a1ebdb5faca3b9c728f41bd
MinJaeSon <mjsonsj@gmail.com>
2025-12-10 23:18:57 +0900

    chore: remove useless code in google login redirection flow

--- src/domains/auth/pages/AuthOAuthCallback.tsx
+++ src/domains/auth/pages/AuthOAuthCallback.tsx
@@ -16,41 +16,6 @@ const AuthOAuthCallback = () => {
 		const searchParams = new URLSearchParams(location.search);
 		const authCode = searchParams.get("code");
 
-		/**
-		 *
-		 */
-		const isInitialLogin = async () => {
-			try {
-				const { data, error } = await apiClient.GET("/api/v1/users/me");
-
-				if (error || !data?.data) {
-					console.error("유저 정보 조회 실패: ", error);
-					return null;
-				}
-
-				return !data.data.nickname;
-			} catch (error) {
-				console.error("유저 정보 조회 실패: ", error);
-				return null;
-			}
-		};
-
-		/**
-		 * 로그인 이후 리다이렉트 처리 (최초 로그인 시 추가 정보 입력 필요)
-		 */
-		const handlePostLoginRedirect = async () => {
-			const redirectUrl = localStorage.getItem("redirectAfterLogin");
-
-			if (await isInitialLogin()) {
-				navigate("/account/create");
-			} else if (redirectUrl) {
-				localStorage.removeItem("redirectAfterLogin");
-				window.location.href = redirectUrl;
-			} else {
-				navigate("/");
-			}
-		};
-
 		const handleAuth = async () => {
 			if (!authCode) {
 				console.error("Authorization code not found");
--- src/domains/auth/pages/AuthOAuthSignUp.tsx
+++ src/domains/auth/pages/AuthOAuthSignUp.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import LoadingView from "@/components/LoadingView";
 
 //
@@ -7,15 +7,12 @@ import LoadingView from "@/components/LoadingView";
 //
 
 const AuthOAuthSignUp = () => {
-  const location = useLocation();
   const navigate = useNavigate();
 
   //
   useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const authCode = searchParams.get("code");
-    navigate("/account/create", { state: { code: authCode } });
-  }, [location.search, navigate]);
+    navigate("/account/create");
+  }, [navigate]);
 
   return (
     <>
