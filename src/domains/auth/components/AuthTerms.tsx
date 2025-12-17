125a5007102ae2ca07fc2e132d8f407455f331c0
MinJaeSon <mjsonsj@gmail.com>
2025-12-17 23:44:12 +0900

    chore: add to check whole term

--- src/domains/auth/components/AuthTerms.tsx
+++ src/domains/auth/components/AuthTerms.tsx
@@ -1,4 +1,5 @@
 import type React from "react";
+import { useEffect, useState } from "react";
 import CheckBox from "@/components/CheckBox";
 import {
 	Accordion,
@@ -24,6 +25,8 @@ interface AuthTermsProps {
 //
 
 const AuthTerms = ({ terms, termChecks, setTermChecks }: AuthTermsProps) => {
+	const [isAllCheck, setIsAllCheck] = useState(true);
+
 	/**
 	 *
 	 */
@@ -52,12 +55,54 @@ const AuthTerms = ({ terms, termChecks, setTermChecks }: AuthTermsProps) => {
 		return term?.isChecked ?? false;
 	};
 
+	/**
+	 *
+	 */
+	const handleAllCheckboxChange = () => {
+		setIsAllCheck(!isAllCheck);
+	};
+
+	//
+	useEffect(() => {
+		setTermChecks((prev) =>
+			prev.map((term) => ({ ...term, isChecked: isAllCheck })),
+		);
+	}, [isAllCheck]);
+
 	return (
 		<Accordion
 			type="single"
 			collapsible
 			className="w-full flex flex-col gap-gap-5"
 		>
+			<AccordionItem value="전체 약관 동의" className="flex flex-col gap-gap-4">
+				<div className="flex items-center w-full justify-between border-0 h-fit">
+					<div className="flex items-center gap-gap-4">
+						<CheckBox
+							checked={isAllCheck}
+							size={20}
+							onChange={handleAllCheckboxChange}
+						/>
+						<p className="typo-body-small font-pretendard text-gray-50">
+							전체 약관 동의
+						</p>
+					</div>
+					<AccordionTrigger className="text-gray-50" />
+				</div>
+				<AccordionContent className="bg-color-gray-5 rounded-radius-medium1 p-padding-8">
+					<span className="typo-body-small-bold font-pretendard">
+						전체 약관 동의
+					</span>
+					<p>&nbsp;</p>
+					<p>전체 약관 동의 시, 아래 항목에 모두 동의한 것으로 간주됩니다.</p>
+					<p>
+						&nbsp;• &nbsp;서비스 이용약관 <br />
+						&nbsp;• &nbsp;개인정보 수집 및 이용 동의 <br />
+						&nbsp;• &nbsp;마케팅 정보 수신 동의 <br />
+					</p>
+				</AccordionContent>
+			</AccordionItem>
+
 			{terms.map((term) => (
 				<AccordionItem
 					key={term.id}
