bfdaa360e31e3c58acf5a3543b6dc61d211c2fd0
JO WONYOUNG <whdnjsdud551@gmail.com>
2026-03-02 14:30:27 +0900

    feat: change empty question set text

--- src/components/EmptyQuestion.tsx
+++ src/components/EmptyQuestion.tsx
@@ -9,7 +9,7 @@ const EmptyQuestion = () => {
 		<div className="h-full w-full flex flex-col justify-center items-center gap-gap-5">
 			<img src={chartClipImage} alt="empty state" className="size-[100px]" />
 			<span className="text-color-gray-30 typo-body-medium">
-				원하는 문제를 생성해볼래요?
+				문제셋을 만들어보세요!
 			</span>
 		</div>
 	);
