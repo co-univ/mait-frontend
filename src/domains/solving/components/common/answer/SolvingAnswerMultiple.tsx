8fc66e8b21b6aee44547bcc44e3ac0b1831e3ade
JO WONYOUNG <whdnjsdud551@gmail.com>
2026-03-04 11:49:24 +0900

    fix: resolve content redered to null in preview modal

--- src/domains/solving/components/common/answer/SolvingAnswerMultiple.tsx
+++ src/domains/solving/components/common/answer/SolvingAnswerMultiple.tsx
@@ -1,26 +1,34 @@
-import type { MultipleChoiceApiResponse } from '@/libs/types';
-import type { SolvingAnswerProps } from './SolvingAnswer';
-import SolvingAnswer from './SolvingAnswer';
+import type { MultipleChoiceApiResponse } from "@/libs/types";
+import type { SolvingAnswerProps } from "./SolvingAnswer";
+import SolvingAnswer from "./SolvingAnswer";
 
 //
 //
 //
 
 interface SolvingAnswerMultipleProps extends SolvingAnswerProps {
-  choice: MultipleChoiceApiResponse
-  onChoiceClick: (choiceNumber: number) => void;
+	choice: MultipleChoiceApiResponse;
+	onChoiceClick: (choiceNumber: number) => void;
 }
 
 //
 //
 //
 
-const SolvingAnswerMultiple = ({ choice, onChoiceClick, ...props }: SolvingAnswerMultipleProps) => {
-  return (
-    <button type="button" onClick={() => onChoiceClick(choice.number)}>
-      <SolvingAnswer {...props} readOnly content={`${choice.number}. ${choice.content}`} />
-    </button>
-  );
+const SolvingAnswerMultiple = ({
+	choice,
+	onChoiceClick,
+	...props
+}: SolvingAnswerMultipleProps) => {
+	return (
+		<button type="button" onClick={() => onChoiceClick(choice.number)}>
+			<SolvingAnswer
+				{...props}
+				readOnly
+				content={`${choice.number}. ${choice.content ?? ""}`}
+			/>
+		</button>
+	);
 };
 
 export default SolvingAnswerMultiple;
