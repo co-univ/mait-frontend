1ba3adaba4ea3f2d98f10cdf9364c5770aedd087
WONYOUNG-HC <whdnjsdud551@gmail.com>
2026-03-17 22:13:15 +0900

    feat: end question set after send winner

--- src/domains/control/hooks/solving/useControlSolvingQuestionSet.ts
+++ src/domains/control/hooks/solving/useControlSolvingQuestionSet.ts
@@ -1,7 +1,12 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { notify } from "@/components/Toast";
+import { MANAGEMENT_ROUTE_PATH } from "@/domains/management/management.routes";
+import useQuestionSets from "@/hooks/useQuestionSets";
+import useTeams from "@/hooks/useTeams";
 import { apiHooks } from "@/libs/api";
 import type { QuestionSetApiResponse } from "@/libs/types";
+import { createPath } from "@/utils/create-path";
 
 //
 //
@@ -24,6 +29,15 @@ interface UseControlSolvingQuestionSetReturn {
 const useControlSolvingQuestionSet = ({
 	questionSetId,
 }: UseControlSolvingQuestionSetProps): UseControlSolvingQuestionSetReturn => {
+	const navigate = useNavigate();
+
+	const { activeTeam } = useTeams();
+
+	const { invalidateQuestionSetsQuery } = useQuestionSets({
+		teamId: activeTeam?.teamId ?? 0,
+		mode: "LIVE_TIME",
+	});
+
 	const { data, refetch } = apiHooks.useQuery(
 		"get",
 		"/api/v1/question-sets/{questionSetId}",
@@ -60,7 +74,20 @@ const useControlSolvingQuestionSet = ({
 			onSuccess: () => {
 				notify.success("문제 풀이가 종료되었습니다.");
 
+				invalidateQuestionSetsQuery();
 				refetch();
+
+				navigate(
+					createPath(
+						MANAGEMENT_ROUTE_PATH.ROOT,
+						{
+							questionSetId,
+						},
+						{
+							mode: "live-time",
+						},
+					),
+				);
 			},
 			onError: () => {
 				notify.error("문제 풀이 종료에 실패했습니다.");
