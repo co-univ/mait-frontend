0eec6ce0695a3a550371e4f16037e42f3d1ea62c
MinJaeSon <mjsonsj@gmail.com>
2026-03-31 00:07:13 +0900

    feat: add winner result button in winner view

--- src/domains/solving/pages/live/SolvingLiveWinner.tsx
+++ src/domains/solving/pages/live/SolvingLiveWinner.tsx
@@ -1,10 +1,12 @@
 import clsx from "clsx";
-import { Award, PartyPopper } from "lucide-react";
+import { Award, ChevronRight, PartyPopper } from "lucide-react";
 import gold_bell from "src/assets/images/gold-bell.png";
 import winner_print from "src/assets/images/winner-print.png";
 import winner_stamp from "src/assets/images/winner-stamp.png";
+import Button from "@/components/Button";
 import useBreakpoint from "@/hooks/useBreakpoint";
 import SolvingFullModalLayout from "../../layouts/live/SolvingFullModalLayout";
+import { useNavigate } from "react-router-dom";
 
 //
 //
@@ -31,7 +33,16 @@ const SolvingLiveWinner = ({
 	open,
 	onClose,
 }: SolvingLiveWinnerProps) => {
+	const navigate = useNavigate();
+
 	const { isMobile } = useBreakpoint();
+
+	/**
+	 * 
+	 */
+	const handleResultClick = () => {
+		navigate("/dashboard");
+	}
 
 	return (
 		<SolvingFullModalLayout open={open} onClose={onClose}>
@@ -60,7 +71,12 @@ const SolvingLiveWinner = ({
 							isMobile ? "w-[312px]" : "w-[400px]",
 						)}
 					/>
-					<div className="flex flex-col items-center absolute left-0 right-0 top-[80px]">
+					<div
+						className={clsx(
+							"flex flex-col items-center absolute left-0 right-0",
+							isMobile ? " top-[56px]" : "top-[80px]",
+						)}
+					>
 						<div className="flex items-center justify-center gap-[20px]">
 							<PartyPopper className="stroke-color-warning-40" />
 							<div
@@ -74,13 +90,13 @@ const SolvingLiveWinner = ({
 							<PartyPopper className="stroke-color-warning-40 -scale-x-100" />
 						</div>
 
-						<div className="h-[32px]" />
+						<div className={clsx(isMobile ? "h-4" : "h-[34px]")} />
 
 						<div className="flex justify-center items-center py-[10px] w-[200px] border-t border-b border-dashed border-color-gray-40 typo-body-small text-color-warning-60">
 							{activeParticipants.length}명 중에 1등하셨습니다
 						</div>
 
-						<div className="h-[34px]" />
+						<div className={clsx(isMobile ? "h-4" : "h-[34px]")} />
 
 						<div className="flex flex-col gap-[10px] items-center">
 							<Award size={40} className="stroke-color-warning-60" />
@@ -94,10 +110,30 @@ const SolvingLiveWinner = ({
 							))}
 						</div>
 
-						<div className="h-[34px]" />
+						<div className={clsx(isMobile ? "h-4" : "h-[34px]")} />
 
 						<div className={clsx(isMobile ? "w-[160px]" : "w-[240px]")}>
 							<img src={winner_stamp} alt="Winner Stamp" />
+						</div>
+
+						<div className={clsx(isMobile ? "h-4" : "h-[34px]")} />
+						
+						<div>
+							<Button
+								className={clsx(
+									"flex flex-col p-padding-6 bg-warning-50 rounded-radius-max border-none text-alpha-white100 gap-gap-5",
+									isMobile
+										? "h-5 typo-body-xsmall w-[181px]"
+										: "typo-body-medium w-[240px]",
+								)}
+								item={
+									<div className="flex items-center">
+										<p>문제풀이 결과 보러가기</p>
+										<ChevronRight />
+									</div>
+								}
+								onClick={handleResultClick}
+							/>
 						</div>
 					</div>
 				</div>
