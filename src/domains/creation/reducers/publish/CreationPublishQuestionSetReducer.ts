9b3d4bc16789a14cc96f2d9140b2579024f283e5
JO WONYOUNG <whdnjsdud551@gmail.com>
2026-05-15 13:40:23 +0900

    feat: send category ids when put question set

--- src/domains/creation/reducers/publish/CreationPublishQuestionSetReducer.ts
+++ src/domains/creation/reducers/publish/CreationPublishQuestionSetReducer.ts
@@ -1,4 +1,5 @@
 import type {
+	QuestionSetCategoryApiResponse,
 	QuestionSetSolveMode,
 	QuestionSetVisibility,
 	UpdateQuestionSetApiRequest,
@@ -8,22 +9,29 @@ import type {
 //
 //
 
-type CreationPublishQuestionSetState = UpdateQuestionSetApiRequest;
+type CreationPublishQuestionSetState = UpdateQuestionSetApiRequest & {
+	categories: QuestionSetCategoryApiResponse[];
+};
 
 type CreationPublishQuestionSetAction =
 	| { type: "SET_TITLE"; payload: string }
 	| { type: "SET_SUBJECT"; payload: string }
 	| { type: "SET_MODE"; payload: QuestionSetSolveMode }
 	| { type: "SET_DIFFICULTY"; payload: string }
-	| { type: "SET_VISIBILITY"; payload: QuestionSetVisibility };
+	| { type: "SET_VISIBILITY"; payload: QuestionSetVisibility }
+	| { type: "SET_CATEGORIES"; payload: QuestionSetCategoryApiResponse[] }
+	| { type: "ADD_CATEGORIES"; payload: QuestionSetCategoryApiResponse }
+	| { type: "REMOVE_CATEGORY"; payload: number };
 
-export const CREATION_PUBLISH_QUESTION_INITIAL_STATE = {
-	title: "",
-	subject: "",
-	solveMode: "LIVE_TIME" as QuestionSetSolveMode,
-	difficulty: "",
-	visibility: "PUBLIC" as QuestionSetVisibility,
-};
+export const CREATION_PUBLISH_QUESTION_INITIAL_STATE: CreationPublishQuestionSetState =
+	{
+		title: "",
+		subject: "",
+		solveMode: "LIVE_TIME" as QuestionSetSolveMode,
+		difficulty: "",
+		visibility: "PUBLIC" as QuestionSetVisibility,
+		categories: [],
+	};
 
 //
 //
@@ -44,6 +52,17 @@ export const creationPublishQuestionSetReducer = (
 			return { ...state, difficulty: action.payload };
 		case "SET_VISIBILITY":
 			return { ...state, visibility: action.payload };
+		case "SET_CATEGORIES":
+			return { ...state, categories: action.payload };
+		case "ADD_CATEGORIES":
+			return { ...state, categories: [...state.categories, action.payload] };
+		case "REMOVE_CATEGORY":
+			return {
+				...state,
+				categories: state.categories.filter(
+					(category) => category.id !== action.payload,
+				),
+			};
 		default:
 			return state;
 	}
